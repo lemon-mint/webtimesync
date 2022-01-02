bbaa4dea32953670963e47e88f9c3dae17654d66
lemon-mint <ice.lemons.mint@gmail.com>
2022-01-02 09:43:10 +0900

    build lib

--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,5 +1,6 @@
-export declare function getTime(): any;
+export declare function getTime(): number;
 export declare function setTimeOffset(offset: number): void;
 export declare function addTimeOffset(offset: number): void;
+export declare function getTimeOffset(): number;
 export declare function syncTime(endpoint?: string, samples?: number): Promise<number>;
 //# sourceMappingURL=index.d.ts.map
